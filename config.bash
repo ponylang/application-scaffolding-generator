9102e6e281d1041dae659dbc30da17031d952c8c
Sean T. Allen <sean@seantallen.com>
2019-10-18 18:52:42 -0400

    Add top level files

---
+++ config.bash
@@ -0,0 +1,35 @@
+#
+# Change each of the variables in this file to the proper value for
+# your application.
+#
+
+# GitHub user or organization that owns the project
+# https://github.com/OWNER/project
+# Unless this application is part of an organization, this should be your username.
+REPO_OWNER="ponylang"
+# Name of the project
+# https://github.com/ponylang/NAME
+REPO_NAME="ponyup"
+# Name of your top-level package
+PACKAGE="ponyup"
+# Description that will appear at the top of your README file
+PROJECT_DESCRIPTION="The Pony toolchain multiplexer."
+# Short description of your project's status.
+# Is it ready for production use? Should no one be using it yet?
+PROJECT_STATUS="${REPO_NAME} is pre-alpha software."
+# GitHub user to access GitHub API as. Will need to provide a
+# personal access token that allows read/write access to the repository.
+# Unless this application is part of an organization, this should be your username.
+GITHUB_USER="ponylang-main"
+# Email address to associate with commits made by GITHUB_USER
+# Unless this application is part of an organization, this should be your email.
+COMMIT_EMAIL="ponylang.main@example.com"
+# Name to associate with commits made by GITHUB_USER
+# Unless this application is part of an organization, this should be your name.
+COMMIT_NAME="Main"
+# Copyright year for the License
+COPYRIGHT_YEAR="2019"
+# Copyright holder for the License
+COPYRIGHT_HOLDER="The Pony Developers"
+# email address that users should use to report code of conduct violations
+COC_EMAIL="coc@example.com"
